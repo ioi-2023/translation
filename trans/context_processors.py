52aa28595429c84e5a37c61edadb0c41b41f4517
Németh Zsolt <37755878+birka0@users.noreply.github.com>
2023-08-07 14:39:53 +0200

    update style to IOI 2023

--- trans/context_processors.py
+++ trans/context_processors.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 def ioi_settings(request):
     return {'settings': {
-        'SITE_TITLE': 'IOI Task Translation System',
+        'SITE_TITLE': 'IOI 2023 Task Translation System',
         'CONTEST_TITLE': 'IOI',
         'TIME_ZONE': settings.TIME_ZONE,
         'IMAGES_URL': '/media/images/',
